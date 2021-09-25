43d02ffd93dfa0b43197a6de86922203ecc019da
Sandesh Kasliwal <sskasliwal95@gmail.com>
2021-09-25 20:30:38 +0530

    feat: codepush integration setup for android

--- android/app/src/main/java/com/wbw/MainApplication.java
+++ android/app/src/main/java/com/wbw/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.microsoft.codepush.react.CodePush;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -39,6 +40,11 @@ public class MainApplication extends Application implements ReactApplication {
   public ReactNativeHost getReactNativeHost() {
     return mReactNativeHost;
   }
+
+  @Override
+      protected String getJSBundleFile() {
+          return CodePush.getJSBundleFile();
+      }
 
   @Override
   public void onCreate() {
