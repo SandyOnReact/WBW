73210f5bb2adc9c94cd076bf7472ac834eca1e68
Sandesh Kasliwal <sandeshk@neuronimbus.com>
2022-01-15 20:44:47 +0530

    feat: implement in app browser support for dynamic POC button

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from 'react'
-import { View, Text, ActivityIndicator, BackHandler, Alert } from 'react-native'
+import { View, Text, ActivityIndicator, BackHandler, Alert, Linking } from 'react-native'
 import Async from 'react-async';
 import { FlatList } from 'react-native-gesture-handler';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -8,6 +8,7 @@ import { DashboardCard } from '../components/dashboard-card'
 import { Divider, Header } from 'react-native-elements'
 import { CommonActions } from '@react-navigation/native';
 import { isEmpty, sortBy } from 'lodash-es';
+import { InAppBrowser } from "react-native-inappbrowser-reborn"
 
 
 export const HomeScreen = ({ navigation }) => {
@@ -44,11 +45,57 @@ export const HomeScreen = ({ navigation }) => {
         return result;
     }, [])
 
+    const openInAppBrowser = async ( ) => {
+        try {
+            const token = await AsyncStorage.getItem('Token')
+            const url = `http://198.71.63.116/MySite/Dashboard.aspx?dp=1&U=${userInfo.UserID}&T=${token}`
+            if (await InAppBrowser.isAvailable()) {
+              const result = await InAppBrowser.openAuth(url, {
+                // iOS Properties
+                dismissButtonStyle: 'cancel',
+                preferredBarTintColor: '#1e5873',
+                preferredControlTintColor: 'white',
+                readerMode: false,
+                animated: true,
+                modalPresentationStyle: 'fullScreen',
+                modalTransitionStyle: 'coverVertical',
+                modalEnabled: true,
+                enableBarCollapsing: false,
+                // Android Properties
+                showTitle: true,
+                toolbarColor: '#1e5873',
+                secondaryToolbarColor: 'black',
+                navigationBarColor: 'black',
+                navigationBarDividerColor: 'white',
+                enableUrlBarHiding: true,
+                enableDefaultShare: true,
+                forceCloseOnRedirection: false,
+                // Specify full animation resource identifier(package:anim/name)
+                // or only resource name(in case of animation bundled with app).
+                animations: {
+                  startEnter: 'slide_in_right',
+                  startExit: 'slide_out_left',
+                  endEnter: 'slide_in_left',
+                  endExit: 'slide_out_right'
+                },
+                headers: {
+                  'my-custom-header': 'my custom header value'
+                }
+              })
+            }
+            else Linking.openURL(url)
+          } catch (error) {
+              console.log( 'error is ',JSON.stringify( error ) )
+            Alert.alert(error.message)
+          }
+    }
+
     const onDashboardPress = (dashboard) => {
         if( dashboard.Category === "POC" ) {
-            navigation.navigate( 'DynamicControls', {
-                dashboard: dashboard
-            } )
+            // navigation.navigate( 'Webview', {
+            //     dashboard: dashboard
+            // } )
+            openInAppBrowser()
         }else if( dashboard.Type === "Audit-originator" ) {
             navigation.navigate( 'AuditAndInspectionScreen', {
                 dashboard: dashboard,
